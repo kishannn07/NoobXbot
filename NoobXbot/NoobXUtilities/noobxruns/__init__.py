36a9ea15461f822f3999724d28ea949eb9c9ef74
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-21 22:52:41 +0530

    Update __init__.py

--- NoobXbot/NoobXUtilities/noobxruns/__init__.py
+++ NoobXbot/NoobXUtilities/noobxruns/__init__.py
@@ -1,9 +1,9 @@
 from os import listdir, mkdir
 from pyrogram import Client
 from NoobXbot import config
-from NoobXbot.NoobXUtilities.queues import (clear, get, is_empty, put, task_done)
-from NoobXbot.NoobXUtilities.downloader import download
-from NoobXbot.NoobXUtilities.convert import convert
-from NoobXbot.NoobXUtilities.music import run
-from NoobXbot.NoobXUtilities.music import smexy as ASS_ACC
+from NoobXbot.NoobXUtilities.noobxruns.queues import (clear, get, is_empty, put, task_done)
+from NoobXbot.NoobXUtilities.noobxruns.downloader import download
+from NoobXbot.NoobXUtilities.noobxruns.convert import convert
+from NoobXbot.NoobXUtilities.noobxruns.music import run
+from NoobXbot.NoobXUtilities.noobxruns.music import smexy as ASS_ACC
 smexy = 1
