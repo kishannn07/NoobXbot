f608b26013f2f8462332f80b17d0cc36243351b9
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-21 20:33:00 +0530

    Update __main__.py

--- NoobXbot/__main__.py
+++ NoobXbot/__main__.py
@@ -7,7 +7,7 @@ from NoobXbot.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USER
 from NoobXbot import BOT_NAME, ASSNAME, app, client
 from NoobXbot.NoobXUtilities.database.functions import clean_restart_stage
 from NoobXbot.NoobXUtilities.database.queue import (get_active_chats, remove_active_chat)
-from NoobXbot.NoobXUtilities.noobxrun import run
+from NoobXbot.NoobXUtilities.noobxruns import run
 from pytgcalls import idle
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import time
